dc64bc40377ba300a230e1b0313b05297d0dca9c
Ar_i1401 <86227684+Achi0114@users.noreply.github.com>
2022-09-08 19:45:37 +0700

    Add files via upload

--- RaspberryPi/MAX30100/testbydcpy.py
+++ RaspberryPi/MAX30100/testbydcpy.py
@@ -20,59 +20,22 @@ def measure(channel):
     mx30.ir, mx30.red
     
     temperature = sensor.get_obj_temp()
-    
+    print("Temp:",temperature);
     hb = int(mx30.ir/100)
     spo2 = int(mx30.red/100)
     spo2 = spo2-3;
     if mx30.ir != mx30.buffer_ir :
         print("HB:",hb);
-        
     if mx30.red != mx30.buffer_red :
         print("SPO2:",spo2);
-        print("Temp:",temperature);
-        lcd.text("HB:"+str(hb)+ " " + "SPO2:"+str(spo2),1)
-        lcd.text("Temp:"+str(temperature),2)
-        response = channel.update({'field1':hb, 'field2' : spo2, 'field3' : temperature })
+    lcd.text("HB:"+str(hb)+ " " + "SPO2:"+str(spo2),1)
+    lcd.text("Temp:"+str(temperature),2)
+    response = channel.update({'field1':hb, 'field2' : spo2, 'field3' : temperature })
     time.sleep(3)
-
-def measure2(channel):
-    
-    
-    temperature = sensor.get_obj_temp()
-    acc_hb = 0
-    acc_spo = 0
-    n_hb = 0
-    n_max = 0
-    n = 0
-    while True:
-        mx30.read_sensor()
-        mx30.ir, mx30.red
-        hb = int(mx30.ir/100)
-        spo2 = int(mx30.red/100)
-        spo2 = spo2-3;
-        if mx30.ir != mx30.buffer_ir :
-            acc_hb = acc_hb + hb
-            n_hb = n_hb + 1
-            
-        if mx30.red != mx30.buffer_red :
-            acc_spo = acc_spo + spo2
-            n_max = n_max + 1
-        time.sleep(0.01)
-        n = n + 1
-        if n > 100:
-            hb = acc_hb / n_hb
-            spo2 = acc_spo /n_max
-            lcd.text("HB:"+str(hb)+ " " + "SPO2:"+str(spo2),1)
-            lcd.text("Temp:"+str(temperature),2)
-            return hb, spo2, temperature
                         
     
 if __name__== "__main__":
     channel = thingspeak.Channel(1844411, write_key)
     while True:
-        #hb, spo2, temperature = measure2(channel)
-        #print("HB:",hb);
-        #print("SPO2:",spo2);
-        #print("Temp:",temperature);
         measure(channel)
         time.sleep(1)
